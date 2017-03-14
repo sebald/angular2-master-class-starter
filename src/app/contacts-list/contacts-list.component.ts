ae0e92a4c689b79486643deeb89e8cd996cfad2d
Sebastian Sebald <sebastian.sebald@gmail.com>
2017-03-14 11:42:15 +0100

    ex9

--- src/app/contacts-list/contacts-list.component.ts
+++ src/app/contacts-list/contacts-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
 import { Contact } from '../models/contact';
 import { ContactService } from '../contacts.service';
 
@@ -8,15 +9,14 @@ import { ContactService } from '../contacts.service';
   styleUrls: ['./contacts-list.component.css']
 })
 export class ContactsListComponent implements OnInit {
-  contacts:Contact[];
+  contacts: Observable<Contact[]>;
 
   constructor(
     private contactService:ContactService
   ) {}
 
   ngOnInit() {
-    this.contactService.getContacts()
-      .subscribe(contacts => this.contacts = contacts)
+    this.contacts = this.contactService.getContacts();
   }
 
   trackByContactId (idx:number, contact:Contact) {
